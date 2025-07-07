24b5a78b54ef56cc81c993b7d59f755295e0f404
Barış Kuyucu <132789393+Bariskycu@users.noreply.github.com>
2025-07-07 23:44:54 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://jewelry-store-6c2e.vercel.app/:3000',
+  credentials: true // if you're using cookies
+}));
 app.use(express.json());
 
 // Read products data
